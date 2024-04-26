5244fc93f3a402e6e3b631d55e7b2fa9aef8f667
Dan Tong <dan@dtong.nz>
2024-04-26 12:53:52 +1200

    add support for multiple distings

--- README.md
+++ README.md
@@ -1,4 +1,12 @@
-# Norns mod. Disting ex nb voice.
+# nb_ex_multi
+
+this is a modified version of [nb_ex](https://github.com/sixolet/nb_ex) that supports multiple distings.
+
+configure the number of devices in `SYSTEM > MODS > NB_EX_MULTI`. after changing the number of devices, restart norns.
+
+you may also need to configure the i2c address on the disting. supported addresses [from here](https://github.com/monome/crow/blob/7027d2971a5491b021d8b309101cf2f75a7b5787/lua/ii/disting.lua#L4) are: 0x41, 0x42, 0x43, 0x44
+
+## original readme
 
 This implementation is pretty simple. I have tested it with the following Disting Ex modes:
 * SD Multisample
--- lib/mod.lua
+++ lib/mod.lua
@@ -1,34 +1,44 @@
-
 local mod = require 'core/mods'
+local menu = require("nb_ex/lib/menu")
+local data_file = _path.data .. mod.this_name .. "/mod.state"
+local device_count
 
 if note_players == nil then
     note_players = {}
 end
 
-local function add_disting_player()
+local function player_name(device_index)
+    if device_count == 1 then
+        return "ex"
+    end
+    return string.format("ex (%d)", device_index)
+end
+
+local function add_disting_player(device_index)
     local player = {
-        count = 0
+        count = 0,
+        name = player_name(device_index),
     }
 
     function player:note_on(note, vel)
         local v_vel = vel * 10
-        local v8 = (note - 60)/12        
-        crow.ii.disting.note_pitch(note, v8)
-        crow.ii.disting.note_velocity(note, v_vel)
+        local v8 = (note - 60)/12
+        crow.ii.disting[device_index].note_pitch(note, v8)
+        crow.ii.disting[device_index].note_velocity(note, v_vel)
     end
 
     function player:pitch_bend(note, amount)
         local v8 = (note + amount - 60)/12
-        crow.ii.disting.note_pitch(note, v8)
+        crow.ii.disting[device_index].note_pitch(note, v8)
     end
 
     function player:note_off(note)
-        crow.ii.disting.note_off(note)
+        crow.ii.disting[device_index].note_off(note)
     end
 
     function player:describe()
         return {
-            name = "ex",
+            name = player.name,
             supports_bend = true,
             supports_slew = false,
             modulate_description = "unsupported",
@@ -36,12 +46,22 @@ local function add_disting_player()
     end
 
     function player:stop_all()
-        -- crow.ii.disting.all_notes_off()
+        -- crow.ii.disting[device_index].all_notes_off()
     end
 
-    note_players["ex"] = player
+    note_players[player_name(device_index)] = player
 end
 
 mod.hook.register("script_pre_init", "nb ex pre init", function()
-    add_disting_player()
+    if util.file_exists(data_file) then
+        device_count = tab.load(data_file).device_count
+    else
+        device_count = 1
+    end
+
+    for device = 1, device_count do
+        add_disting_player(device)
+    end
 end)
+
+mod.menu.register(mod.this_name, menu)
